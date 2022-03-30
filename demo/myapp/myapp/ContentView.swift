c1460adb9b69adbd8dbb3f97da3c40de70c649b4
lokilontan <lokilontan@yandex.ru>
2022-03-29 22:24:30 -0700

    first ccommit

---
+++ demo/myapp/myapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myapp
+//
+//  Created by Vladimir Plagov on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo/myapp/myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by Vladimir Plagov on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
