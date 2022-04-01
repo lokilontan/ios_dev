78ca25e2924f7d77095c88c73ca9d607c5099194
lokilontan <lokilontan@yandex.ru>
2022-04-01 00:44:03 -0700

    another "Groups" commit

--- group/group/GroupCardView.swift
+++ group/group/GroupCardView.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct GroupCardView: View {
+    var groupName = "New Group"
+    var groupLabelColor: Color = .green
+    var hstackSpacing: CGFloat = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0){
+            groupLabelColor
+                .frame(width: 10)
+            VStack(alignment: .leading,
+                   spacing: hstackSpacing){
+                HStack{
+                    //icons
+                    Image(systemName: "shippingbox")
+                    Image(systemName: "calendar")
+                    Image(systemName: "exclamationmark.2")
+                        .foregroundColor(.red)
+                }
+                Text(groupName)
+                    .fontWeight(.bold)
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .padding()
+        }
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(10)
     }
 }
 
 struct GroupCardView_Previews: PreviewProvider {
     static var previews: some View {
-        GroupCardView()
+        VStack {
+//        GroupCardView()
+//            .frame(width: 200, height:150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//            .border(Color.red, width: 0)
+        
+            GroupCardView()
+                .frame(width: 200, height:150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            GroupCardView()
+                .frame(width: 300, height:150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            GroupCardView(groupLabelColor: .red)
+                .frame(width: 200, height:150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            GroupCardView(groupName: "ECS 198F")
+                .frame(width: 100, height:150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        
+        }
+        
     }
 }
